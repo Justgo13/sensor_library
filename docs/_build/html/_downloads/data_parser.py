f786a1baa728861e0f440a315fe858349b359673
Jason Gao <jgao2299@gmail.com>
2019-06-19 20:16:58 -0400

    Made it so that data parser functions require input of .csv name as well

--- docs/_build/html/_downloads/data_parser.py
+++ docs/_build/html/_downloads/data_parser.py
@@ -3,21 +3,28 @@ import matplotlib.pyplot as plt
 import csv
 
 
-def iq_data(filename):
+def iq_data(filename,csvname):
     """
-    Takes binary data file and iterates through in-phase and quadrature values, real and imaginary.
-    Data from 180 range bins is taken and in-phase value are matched with their quadrature values.
+    Takes binary data file and iterates through in-phase (real) and quadrature (imaginary) values.
+    Data from range bins is taken and in-phase values are matched with quadrature values to be stored in a user defined .csv file.
+
+    :parameter:
+
+    filename: str
+        The .dat binary file name.
+    csvname: str
+        User defined .csv file name
 
     :example:
 
-    >>> iqdata(filename)
-    >>> 1
+    >>> iqdata('data.dat')
+    >>> 'converted'
 
     :returns:
 
     In-phase and quadrature pairs stored together in a .csv file.
     """
-    with open('data.dat', "rb") as f:
+    with open(filename, "rb") as f:
         data = np.fromfile(f, dtype=np.float32)
     for i in range(0, len(data) // 363 - 1):
         temp = data[3 * (i + 1) + 360 * i:3 * (i + 1) + 360 * (i + 1)]
@@ -28,36 +35,43 @@ def iq_data(filename):
             else:
                 iqdata.append(str(round(temp[j], 4)) + str(round(temp[j + 180], 4)) + "j")
 
-        with open('iqdata.csv', 'a', newline="") as csvFile:
+        with open(csvname+'.csv', 'a', newline="") as csvFile:
             writer = csv.writer(csvFile)
             writer.writerow(iqdata)
     f.close()
     csvFile.close()
-    return 1
+    print('converted')
 
 
-def raw_data(filename):
+def raw_data(filename,csvname):
     """
-    Takes raw data file and iterates through in-phase and quadrature values, real and imaginary.
-    Data from 180 range bins is taken and in-phase value are put apart from quadrature matches.
+    Takes raw data file and iterates through in-phase (real) and quadrature (imaginary) values.
+    Data from range bins is taken, and in-phase value are put apart from quadrature in a user defined .csv file.
+
+    :parameter
+
+    filename: str
+        The .dat binary file name.
+    csvname: str
+        User defined .csv file name
 
     :example:
 
-    >>> raw_data(filename)
-    >>> 1
+    >>> raw_data('data.dat')
+    >>> 'converted'
 
     :returns:
 
     In-phase and quadrature stored separately in a .csv file.
     """
-    with open('data.dat', "rb") as f:
+    with open(filename, "rb") as f:
         data = np.fromfile(f, dtype=np.float32)
     for i in range(0, len(data) // 1473 - 1):
         temp = data[3 + 1470 * i:3 + 1470 * (i + 1)]
 
-        with open('rawdata.csv', 'a', newline="") as csvFile:
+        with open(csvname+'.csv', 'a', newline="") as csvFile:
             writer = csv.writer(csvFile)
             writer.writerow(temp)
     f.close()
     csvFile.close()
-    return 1
+    print('converted')
